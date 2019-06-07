c0a2cfc5bd3f4023bca6b137d13a3a0adc558dbf
Jose Manuel <josemanuelcuchimbavargas@gmail.com>
2019-06-06 22:27:31 -0500

    Validaciones corregidas y funcionalidades agregadas

--- src/app/components/salary/createsalary/createsalary.component.ts
+++ src/app/components/salary/createsalary/createsalary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 // import { EventEmitter } from 'protractor';
 import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { SalaryService } from '../../../services/salary.service';
@@ -11,10 +11,17 @@ let domModalSalario;
 })
 export class CreatesalaryComponent implements OnInit {
 
+
+  value = '';
   salary: any = new Object();
   isAlertVisible = false;
   @Input() eventEdit = new Object();
   @Output() isResult = new EventEmitter();
+  @ViewChild('salaryBasic') salaryBasicElement: ElementRef;
+  @ViewChild('auxilioTransporte') auxilioTransporteElement: ElementRef;
+  @ViewChild('prestacionSocial') prestacionSocialElement: ElementRef;
+  @ViewChild('year') yearElement: ElementRef;
+
   constructor(
     private salaryService: SalaryService) { }
 
@@ -24,42 +31,17 @@ export class CreatesalaryComponent implements OnInit {
   }
 
   saveSalary() {
-
-    if(isNaN(this.salary.salario )){
-      swal("Advertencia!", "Campo salario solo permite numero.", "warning");
-      return false;
-    } else {
-      this.salary.salario = Number(this.salary.salario);
-    }       
-
-    if(isNaN(this.salary.auxilioTransporte)){
-      swal("Advertencia!", "Campo Auxilio Transporte solo permite numero.", "warning");
-      return false;
-    } else {
-      this.salary.auxilioTransporte = Number(this.salary.auxilioTransporte);
-    }             
-
-    if(isNaN(this.salary.prestacionesSociales)){
-      swal("Advertencia!", "Campo Prestaciones Sociales solo permite numero.", "warning");
-      return false;
-    } else {
-      this.salary.prestacionesSociales = Number(this.salary.prestacionesSociales);
-    }   
-    
-    if(isNaN(this.salary.periodo)){
-      swal("Advertencia!", "Campo año solo permite numero.", "warning");
-      return false;
-    } else {
-      this.salary.periodo = Number(this.salary.periodo);
-    }     
-
+    this.salary.salario = this.salary.salario.toString();   
+    this.salary.auxilioTransporte = this.salary.auxilioTransporte.toString();
+    this.salary.prestacionesSociales = this.salary.prestacionesSociales.toString();    
+    this.salary.periodo = this.salary.periodo;
     this.salaryService.createSalary(this.salary).subscribe((result: any) => {
-      this.isAlertVisible = true;
-      let entorno = this;
-      setTimeout(function () {
-        entorno.salary = new Object();
-        entorno.isResult.emit(true);
-      }, 1000);
+      // this.isAlertVisible = true;
+        let entorno = this;
+          swal("Petición correcta!","","success").then(()=>{
+              entorno.salary = new Object();
+              entorno.isResult.emit(true);
+          });
     });
 
   }
@@ -75,6 +57,42 @@ export class CreatesalaryComponent implements OnInit {
     this.salary.periodo = dataEdit.periodo;    
     this.salary.estado = dataEdit.estado;    
   }
+
+  onChange(value: string, tipo: number): void {
+    this.updateValue(value, tipo);
+  }
+
+  // '.' at the end or only '-' in the input box.
+  onBlur(): void {
+    if (this.value.charAt(this.value.length - 1) === '.' || this.value === '-') {
+      this.updateValue(this.value.slice(0, -1));
+    }
+  }
+
+  updateValue(value: string, tipo?: number): void {
+    const reg = /^-?(0|[1-9][0-9]*)(\.[0-9]*)?$/;
+    if ((!isNaN(+value) && reg.test(value)) || value === '' || value === '-') {
+      this.value = value;
+    }
+    switch (tipo) {
+      case 1:
+            this.salaryBasicElement.nativeElement.value = this.value;      
+        break;
+        case 2:
+            this.auxilioTransporteElement.nativeElement.value = this.value;      
+        break;
+        case 3:
+            this.prestacionSocialElement.nativeElement.value = this.value;      
+        break;
+        case 4:
+            this.yearElement.nativeElement.value = this.value;              
+        break;    
+      default:
+        break;
+    }
+    
+  }
+
 
 
 }
