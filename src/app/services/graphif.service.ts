5e4da69c5358a7f0384deeef9d8963b719c0d890
paulaj <paujaramillos@gmail.com>
2019-06-22 18:25:59 -0500

    update design component

--- src/app/services/graphif.service.ts
+++ src/app/services/graphif.service.ts
@@ -24,7 +24,7 @@ export class GraphifService {
   }
 
   getData() {
-    return this.db.list('/monitor', ref => ref.limitToFirst(100)).valueChanges()
+    return this.db.list('/monitor', ref => ref.limitToFirst(10)).valueChanges()
   }
 
 }
