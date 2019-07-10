00024d0d38cb111c6b0660eb7e69e3af0c88345c
paulaj <paujaramillos@gmail.com>
2019-07-10 00:14:08 -0500

    Update model producto

--- src/app/models/dtProducto.model.ts
+++ src/app/models/dtProducto.model.ts
@@ -1,10 +1,13 @@
 export class DtProducto {
     iddtProducto: string;
     fechaMovimiento: string;
+    codigo: string;
     descripcion: string;
-    costoProducto: string;
-    entrada: string;
-    salida: string;  
-    cantidadTotal: string;  
-    valorTotal: string;  
+    tipoMovimiento: string;
+    cantidadUnitaria: string;  
+    promedioUnitario: string;
+    totalUnitario: string;
+    promedioTotal: string;
+    cantidadTotal: string;
+    valorTotal: string;
 }
--- src/app/models/producto.model.ts
+++ src/app/models/producto.model.ts
@@ -1,10 +1,11 @@
 import { DtProducto } from './dtProducto.model';
+import {Marca} from './mark.model';
 
 export class Producto {
     idproducto: string;
     codProducto: string;
     nombreProducto: string;
-    marcaProducto: string;
+    marcaProducto: Marca;
     unidadMedida: string;
     fechaVencimiento: string;  
     idtipoProducto: string;  
