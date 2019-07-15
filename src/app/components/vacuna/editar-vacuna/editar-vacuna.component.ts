3b9c1fa9c4ebb90ab880e1f46d817ad356f7da73
paulaj <paujaramillos@gmail.com>
2019-07-14 19:58:56 -0500

    Updata create update vacuna

--- src/app/components/vacuna/editar-vacuna/editar-vacuna.component.ts
+++ src/app/components/vacuna/editar-vacuna/editar-vacuna.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
     selector: 'app-editar-vacuna',
     templateUrl: './editar-vacuna.component.html',
     styleUrls: ['./editar-vacuna.component.css']
-  })
-  export class EditarVacunaComponent implements OnInit { 
+})
+export class EditarVacunaComponent implements OnInit {
 
-    constructor() {
+    constructor(private route: ActivatedRoute,
+        private router: Router,
+    ) {
 
     }
 
     ngOnInit() {
-
+        this.getData()
     }
-  }
+
+    getData() {
+        this.route.params.subscribe(params => {
+            console.log('params router', params)
+        })
+    }
+}
